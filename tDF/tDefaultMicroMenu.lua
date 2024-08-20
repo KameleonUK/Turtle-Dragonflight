b3a9337690e080525764e744223fc1c0c3f20e51
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-20 02:02:16 +0000

    Add files via upload

--- tDF/tDefaultMicroMenu.lua
+++ tDF/tDefaultMicroMenu.lua
@@ -5,24 +5,34 @@ This lua file moves the default buttons.
 ]]
 HelpMicroButton:ClearAllPoints()
 HelpMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -10, 0)
+HelpMicroButton:Hide()
 
 MainMenuMicroButton:ClearAllPoints()
 MainMenuMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -35, 0)
+MainMenuMicroButton:Hide()
 
 WorldMapMicroButton:ClearAllPoints()
 WorldMapMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -60, 0)
+WorldMapMicroButton:Hide()
 
 SocialsMicroButton:ClearAllPoints()
 SocialsMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -85, 0)
+SocialsMicroButton:Hide()
 
 QuestLogMicroButton:ClearAllPoints()
 QuestLogMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -110, 0)
+QuestLogMicroButton:Hide()
 
 TalentMicroButton:ClearAllPoints()
-TalentMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -135, 0)
+--TalentMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -135, 0)
+TalentMicroButton:Hide()
+TalentMicroButton:SetPoint("BOTTOMRIGHT", UIParent, 500, 0)
+
 
 SpellbookMicroButton:ClearAllPoints()
 SpellbookMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -160, 0)
+SpellbookMicroButton:Hide()
 
 CharacterMicroButton:ClearAllPoints()
 CharacterMicroButton:SetPoint("BOTTOMRIGHT", UIParent, -185, 0)
+CharacterMicroButton:Hide()
